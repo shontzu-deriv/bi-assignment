d6a563428b3a0c07a17516d68bb0e93b1365a41f
shontzu alias yuuchin <shon.tzu@besquare.com.my>
2022-10-20 22:57:00 +0000

    q5

---
+++ q5.py
@@ -0,0 +1,74 @@
+from datetime import timedelta
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+def send_email_basic(sender, receiver, email_subject):
+    # variables
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = sender  # Enter your address
+    receiver_email = receiver  # Enter receiver address
+    password = 'env variable'  # Enter your gmail password
+    email_html = """
+    <html>
+    <body>
+        <p>Hello!</p>
+        <p>Add any text you'd like to the body of the e-mail here!</p>   <br>
+    </body>
+    </html>
+    """
+    message = MIMEMultipart("multipart")
+    # Turn these into plain/html MIMEText objects
+    part2 = MIMEText(email_html, "html")
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part2)
+    message["Subject"] = email_subject
+    message["From"] = sender_email
+    
+    
+    # iterating through the receiver list
+    for i, val in enumerate(receiver):
+        message["To"] = val
+        context = ssl.create_default_context()
+        with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+            server.login(sender_email, password)
+            server.sendmail(sender_email, receiver_email, message.as_string())
+
+
+def init_email():
+    sender = "hello@gmail.com"  # add the sender gmail address here
+    # add your e-mail recipients here
+    recipients = ["alice@gmail.com", "bob@gmail.com"]
+    subject = "hacker"  # add the subject of the e-mail you'd like here
+    send_email_basic(sender, recipients, subject)
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': 'trigger date',
+    'email': ['your Airflow email'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 0
+}
+
+dag = DAG('email_automation',
+          default_args=default_args,
+          dagrun_timeout=timedelta(minutes=5),
+          # schedule_interval = '0 13 * * 1', # if you want to set up an automation schedule you can do that here
+          catchup=False
+          )
+t1 = PythonOperator(
+    task_id='send_email',
+    python_callable=init_email,
+    dag=dag
+)
+
+
+t1
